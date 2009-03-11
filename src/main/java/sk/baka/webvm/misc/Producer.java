8b5625218fb0937f4dc3225b7c615af2ebc9e3b7
Martin Vysny <mvy@whitestein.com>
2009-03-11 15:10:58 +0100

    SysInfo made more effective: the property/env map is no longer part of the page state.

---
+++ src/main/java/sk/baka/webvm/misc/Producer.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2009 Martin Vysny.
+ *
+ * This file is part of WebVM.
+ *
+ * WebVM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WebVM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WebVM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package sk.baka.webvm.misc;
+
+import java.io.Serializable;
+
+/**
+ * Produces objects of required type.
+ * @author Martin Vysny
+ */
+public interface Producer<T> extends Serializable {
+
+    /**
+     * Produce an item.
+     * @return the item.
+     */
+    T produce();
+}
