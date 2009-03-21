6090ca35ca0f4ae8b60deea23c7af585a5e2ec6c
Martin Vysny <vysny@baka.sk>
2009-03-21 16:18:23 +0100

    minor

--- src/main/java/sk/baka/webvm/SysInfo.java
+++ src/main/java/sk/baka/webvm/SysInfo.java
@@ -21,7 +21,6 @@ package sk.baka.webvm;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.apache.wicket.PageParameters;
