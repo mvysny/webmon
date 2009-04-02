68c9edc6ccf6e68d8adce247fcbf06f024c0d332
Martin Vysny <vysny@baka.sk>
2009-04-02 22:03:19 +0200

    Implemented a search feature to ResourceLink; added tests for both JAR and DIR ResourceLink

---
+++ src/test/java/sk/baka/webvm/analyzer/classloader/JarResourceLinkTest.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2009 Martin Vysny.
+ *
+ * This file is part of WebVM.
+ *
+ * WebVM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WebVM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WebVM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package sk.baka.webvm.analyzer.classloader;
+
+import java.io.File;
+
+/**
+ * Runs the tests on a jar file.
+ * @author Martin Vysny
+ */
+public class JarResourceLinkTest extends AbstractResourceLinkTest {
+
+    @Override
+    protected File getFile() {
+        return new File("src/test/files/sunjce_provider.jar");
+    }
+}
