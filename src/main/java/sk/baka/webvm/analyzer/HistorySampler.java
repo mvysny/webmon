58db452c380f7461c4f11586aa133c8806265d98
Martin Vysny <mvy@whitestein.com>
2009-04-27 15:12:21 +0200

    GC CPU measurement now implemented as ICpuUsageMeasure service

--- src/main/java/sk/baka/webvm/analyzer/HistorySampler.java
+++ src/main/java/sk/baka/webvm/analyzer/HistorySampler.java
@@ -18,7 +18,6 @@
  */
 package sk.baka.webvm.analyzer;
 
-import sk.baka.webvm.misc.*;
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.util.List;
@@ -27,6 +26,9 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import sk.baka.webvm.config.Config;
+import sk.baka.webvm.misc.BackgroundService;
+import sk.baka.webvm.misc.NotificationDelivery;
+import sk.baka.webvm.misc.SimpleFixedSizeFIFO;
 
 /**
  * Samples the VM history regularly. You need to invoke {@link #start()} to start the sampler, {@link #stop()} to stop it. Thread-safe.
@@ -34,42 +36,41 @@ import sk.baka.webvm.config.Config;
  */
 public final class HistorySampler extends BackgroundService {
 
-	private static final Logger log = Logger.getLogger(HistorySampler.class.getName());
-	private final ProblemAnalyzer analyzer;
+    private static final Logger log = Logger.getLogger(HistorySampler.class.getName());
+    private final ProblemAnalyzer analyzer;
 
-	/**
-	 * Creates new sampler instance with default values.
-	 * @param analyzer a configured instance of the analyzer
-	 */
-	public HistorySampler(final ProblemAnalyzer analyzer) {
-		this(HISTORY_VMSTAT, HISTORY_PROBLEMS, analyzer);
-	}
+    /**
+     * Creates new sampler instance with default values.
+     * @param analyzer a configured instance of the analyzer
+     */
+    public HistorySampler(final ProblemAnalyzer analyzer) {
+        this(HISTORY_VMSTAT, HISTORY_PROBLEMS, analyzer);
+    }
 
-	/**
-	 * Creates new sampler instance.
-	 * @param vmstatConfig the vmstat sampler config
-	 * @param problemConfig the problem sampler config
-	 * @param analyzer a configured instance of the analyzer
-	 */
-	public HistorySampler(final SamplerConfig vmstatConfig, final SamplerConfig problemConfig, final ProblemAnalyzer analyzer) {
+    /**
+     * Creates new sampler instance.
+     * @param vmstatConfig the vmstat sampler config
+     * @param problemConfig the problem sampler config
+     * @param analyzer a configured instance of the analyzer
+     */
+    public HistorySampler(final SamplerConfig vmstatConfig, final SamplerConfig problemConfig, final ProblemAnalyzer analyzer) {
         super("Sampler", 1);
-		this.vmstatConfig = vmstatConfig;
-		this.problemConfig = problemConfig;
-		vmstatHistory = new SimpleFixedSizeFIFO<HistorySample>(vmstatConfig.getHistoryLength());
-		problemHistory = new SimpleFixedSizeFIFO<List<ProblemReport>>(problemConfig.getHistoryLength());
-		this.analyzer = analyzer;
-	}
-	private volatile Config config = new Config();
-	private final SamplerConfig problemConfig;
-	/**
-	 * Default VMStat history.
-	 */
-	public static final SamplerConfig HISTORY_VMSTAT = new SamplerConfig(150, 1000, 0);
-	/**
-	 * Default Problems history.
-	 */
-	public static final SamplerConfig HISTORY_PROBLEMS = new SamplerConfig(20, 10 * 1000, 500);
-
+        this.vmstatConfig = vmstatConfig;
+        this.problemConfig = problemConfig;
+        vmstatHistory = new SimpleFixedSizeFIFO<HistorySample>(vmstatConfig.getHistoryLength());
+        problemHistory = new SimpleFixedSizeFIFO<List<ProblemReport>>(problemConfig.getHistoryLength());
+        this.analyzer = analyzer;
+    }
+    private volatile Config config = new Config();
+    private final SamplerConfig problemConfig;
+    /**
+     * Default VMStat history.
+     */
+    public static final SamplerConfig HISTORY_VMSTAT = new SamplerConfig(150, 1000, 0);
+    /**
+     * Default Problems history.
+     */
+    public static final SamplerConfig HISTORY_PROBLEMS = new SamplerConfig(20, 10 * 1000, 500);
     private final NotificationDelivery notificator = new NotificationDelivery();
 
     /**
@@ -82,104 +83,108 @@ public final class HistorySampler extends BackgroundService {
     }
 
     @Override
-	protected void started(final ScheduledExecutorService executor) {
+    protected void started(final ScheduledExecutorService executor) {
         notificator.start();
-		executor.scheduleWithFixedDelay(new Sampler(), vmstatConfig.getInitialDelay(), vmstatConfig.getHistorySampleDelayMs(), TimeUnit.MILLISECONDS);
-		executor.scheduleWithFixedDelay(new ProblemSampler(), problemConfig.getInitialDelay(), problemConfig.getHistorySampleDelayMs(), TimeUnit.MILLISECONDS);
-	}
+        executor.scheduleWithFixedDelay(new Sampler(), vmstatConfig.getInitialDelay(), vmstatConfig.getHistorySampleDelayMs(), TimeUnit.MILLISECONDS);
+        executor.scheduleWithFixedDelay(new ProblemSampler(), problemConfig.getInitialDelay(), problemConfig.getHistorySampleDelayMs(), TimeUnit.MILLISECONDS);
+    }
 
     @Override
     protected void stopped() {
         notificator.stop();
     }
+    private final SimpleFixedSizeFIFO<HistorySample> vmstatHistory;
 
-	private final SimpleFixedSizeFIFO<HistorySample> vmstatHistory;
+    /**
+     * Returns a snapshot of the history values.
+     * @return modifiable snapshot.
+     */
+    public List<HistorySample> getVmstatHistory() {
+        return vmstatHistory.toList();
+    }
 
-	/**
-	 * Returns a snapshot of the history values.
-	 * @return modifiable snapshot.
-	 */
-	public List<HistorySample> getVmstatHistory() {
-		return vmstatHistory.toList();
-	}
+    private final class Sampler implements Runnable {
 
-	private final class Sampler implements Runnable {
+        public void run() {
+            try {
+                final int cpuUsageByGC = gcCpuUsage.getCpuUsage();
+                vmstatHistory.add(new HistorySample(cpuUsageByGC));
+            } catch (Throwable e) {
+                log.log(Level.SEVERE, "The Sampler thread failed", e);
+            }
+        }
+    }
+    private SamplerConfig vmstatConfig;
+    private final SimpleFixedSizeFIFO<List<ProblemReport>> problemHistory;
+    private final CpuUsage gcCpuUsage = new CpuUsage(new GCCpuUsageMeasure());
 
-		private long lastGcTimes = -1;
-		private long lastGcSampleTaken = -1;
+    /**
+     * Measures the GC CPU usage.
+     */
+    private static final class GCCpuUsageMeasure implements ICpuUsageMeasure {
 
-		public void run() {
-			try {
-				final int cpuUsageByGC = getGCCPUUsage();
-				vmstatHistory.add(new HistorySample(cpuUsageByGC));
-			} catch (Throwable e) {
-				log.log(Level.SEVERE, "The Sampler thread failed", e);
-			}
-		}
+        public boolean supported() {
+            return true;
+        }
 
-		private int getGCCPUUsage() {
-			// get the GC CPU usage
-			long collectTime = 0;
-			final List<GarbageCollectorMXBean> beans = ManagementFactory.getGarbageCollectorMXBeans();
-			if (beans != null) {
-				for (final GarbageCollectorMXBean bean : beans) {
-					if (!bean.isValid()) {
-						continue;
-					}
-					if (bean.getCollectionTime() > 0) {
-						collectTime += bean.getCollectionTime();
-					}
-				}
-			}
-			final long currentTimeMillis = System.currentTimeMillis();
-			if (lastGcTimes < 0) {
-				lastGcTimes = collectTime;
-				lastGcSampleTaken = currentTimeMillis;
-			}
-			final long gcTimeDelta = collectTime - lastGcTimes;
-			final long gcSampleTakenDelta = currentTimeMillis - lastGcSampleTaken;
-			lastGcTimes = collectTime;
-			lastGcSampleTaken = currentTimeMillis;
-			final int cpuUsageByGC;
-			if (gcSampleTakenDelta > 0) {
-				cpuUsageByGC = (int) (gcTimeDelta * 100 / gcSampleTakenDelta);
-			} else {
-				cpuUsageByGC = 0;
-			}
-			return cpuUsageByGC;
-		}
-	}
-	private SamplerConfig vmstatConfig;
-	private final SimpleFixedSizeFIFO<List<ProblemReport>> problemHistory;
+        public Object measure() throws Exception {
+            // get the GC CPU usage
+            long collectTime = 0;
+            final List<GarbageCollectorMXBean> beans = ManagementFactory.getGarbageCollectorMXBeans();
+            if (beans != null) {
+                for (final GarbageCollectorMXBean bean : beans) {
+                    if (!bean.isValid()) {
+                        continue;
+                    }
+                    if (bean.getCollectionTime() > 0) {
+                        collectTime += bean.getCollectionTime();
+                    }
+                }
+            }
+            final long currentTimeMillis = System.currentTimeMillis();
+            return new long[]{collectTime, currentTimeMillis};
+        }
 
-	private final class ProblemSampler implements Runnable {
+        public int getAvgCpuUsage(Object o1, Object o2) {
+            final long[] m1 = (long[]) o1;
+            final long[] m2 = (long[]) o2;
+            final long gcTimeDelta = m2[0] - m1[0];
+            final long gcSampleTakenDelta = m2[1] - m1[1];
+            if (gcSampleTakenDelta == 0) {
+                return 0;
+            }
+            return (int) (gcTimeDelta * 100 / gcSampleTakenDelta);
+        }
+    }
 
-		public void run() {
-			try {
-				final List<ProblemReport> currentProblems = analyzer.getProblems(vmstatHistory.toList());
-				final List<ProblemReport> last = problemHistory.getNewest();
-				if (last == null) {
-					if (!ProblemReport.isProblem(currentProblems)) {
-						return;
-					}
-				} else {
-					if (ProblemReport.equals(last, currentProblems)) {
-						return;
-					}
-				}
-				problemHistory.add(currentProblems);
-				notificator.deliverAsync(currentProblems);
-			} catch (Throwable e) {
-				log.log(Level.SEVERE, "The ProblemSampler timer failed", e);
-			}
-		}
-	}
+    private final class ProblemSampler implements Runnable {
+
+        public void run() {
+            try {
+                final List<ProblemReport> currentProblems = analyzer.getProblems(vmstatHistory.toList());
+                final List<ProblemReport> last = problemHistory.getNewest();
+                if (last == null) {
+                    if (!ProblemReport.isProblem(currentProblems)) {
+                        return;
+                    }
+                } else {
+                    if (ProblemReport.equals(last, currentProblems)) {
+                        return;
+                    }
+                }
+                problemHistory.add(currentProblems);
+                notificator.deliverAsync(currentProblems);
+            } catch (Throwable e) {
+                log.log(Level.SEVERE, "The ProblemSampler timer failed", e);
+            }
+        }
+    }
 
     /**
      * Returns a snapshot view over the problem history.
      * @return the history.
      */
     public List<List<ProblemReport>> getProblemHistory() {
-		return problemHistory.toList();
-	}
+        return problemHistory.toList();
+    }
 }
