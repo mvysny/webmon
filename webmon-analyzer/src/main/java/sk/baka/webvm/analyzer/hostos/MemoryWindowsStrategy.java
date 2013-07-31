1f8c87693742d5bc40860dfc3140c94aaac94393
Martin Vysny <vysny@baka.sk>
2013-07-31 18:16:56 +0200

    Windows swap is now detected using WMI

---
+++ webmon-analyzer/src/main/java/sk/baka/webvm/analyzer/hostos/MemoryWindowsStrategy.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2009 Martin Vysny.
+ *
+ * This file is part of WebMon.
+ *
+ * WebMon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WebMon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WebMon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package sk.baka.webvm.analyzer.hostos;
+
+import java.lang.management.MemoryUsage;
+import sk.baka.webvm.analyzer.utils.WMIUtils;
+
+/**
+ * Provides Windows memory data from the WMI.
+ * @author Martin Vysny
+ */
+public class MemoryWindowsStrategy implements IMemoryInfoProvider {
+
+    public static boolean isAvailable() {
+        return WMIUtils.isAvailable();
+    }
+    
+    public MemoryUsage getSwap() {
+        if (WMIUtils.isAvailable()) {
+            return WMIUtils.getSwapUsage();
+        }
+        return null;
+    }
+
+    public MemoryUsage getPhysicalMemory() {
+        // fallback to JMX
+        return new MemoryJMXStrategy().getPhysicalMemory();
+    }
+}
