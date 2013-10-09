298f37a3feeca0523a5a8039313d9b5b89dbcaf5
Martin Vysny <martin.vysny@innovatrics.com>
2013-10-09 17:14:53 +0200

    Threads: join/awaitTermination now uses Duration; added ThreadFactory producer which uses ServiceName naming convention

--- webmon-analyzer/src/main/java/sk/baka/webvm/analyzer/utils/Checks.java
+++ webmon-analyzer/src/main/java/sk/baka/webvm/analyzer/utils/Checks.java
@@ -5,9 +5,10 @@ package sk.baka.webvm.analyzer.utils;
  * @author Martin Vysny
  */
 public class Checks {
-    public static void checkNotNull(String name, Object value) {
+    public static <T> T checkNotNull(String name, T value) {
         if (value == null) {
             throw new IllegalArgumentException("Parameter name: invalid value " + name + ": must not be null");
         }
+        return value;
     }
 }
